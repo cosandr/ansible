9662a8075350c71eeb5374506ca73cc87bb6fdaa
Andrei Costescu <andrei@costescu.no>
2021-12-23 21:43:27 +0100

    Fix pre-commit hook when using BSD find

--- git-init.sh
+++ git-init.sh
@@ -8,7 +8,14 @@ hook_path="$(git rev-parse --git-path hooks)/pre-commit"
 cat <<EOT > "$hook_path"
 #!/usr/bin/env bash
 
-find files host_vars group_vars -type f -regextype egrep -regex '.*(\.csr|\.key|vault\.yml)$' -print0 | while read -d $'\0' f
+# Do we have GNU find?
+if find --version &>/dev/null; then
+    cmd="find files host_vars group_vars -type f -regextype egrep -regex '.*(\.csr|\.key|vault\.yml)$' -print0"
+else
+    cmd="find -E files host_vars group_vars -type f -regex '.*(\.csr|\.key|vault\.yml)$' -print0"
+fi
+
+eval \$cmd | while read -d $'\0' f
 do
     if ! grep -q "\$ANSIBLE_VAULT;" "\$f"; then
         echo "[38;5;208m\$f not encrypted! [0m"
