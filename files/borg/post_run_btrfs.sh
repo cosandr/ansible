69ca6fec21d89badd0a856aaf7fa4783cec01976
Andrei Costescu <andrei@costescu.no>
2022-12-31 19:34:57 +0100

    Fix borg btrfs nested snapshots

--- files/borg/post_run_btrfs.sh
+++ files/borg/post_run_btrfs.sh
@@ -1,4 +1,9 @@
 cd /
+# Set snapshots RW so we can delete nested ones
+for p in "${snap_subvols[@]}"; do
+    btrfs property set -ts "$snap_dir/$p" ro false
+done
+
 for (( idx=${#snap_subvols[@]}-1 ; idx>=0 ; idx-- )) ; do
     btrfs subvolume delete "$snap_dir/${snap_subvols[idx]}"
 done
