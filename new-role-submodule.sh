df4c6d9aeb8962ceac13f91dded0af6881152a81
Andrei Costescu <andrei@costescu.no>
2024-06-09 20:49:41 +0200

    Make new-role submodules optional

---
+++ new-role-submodule.sh
@@ -0,0 +1,1 @@
+new-role.sh
--- new-role.sh
+++ new-role.sh
@@ -4,15 +4,26 @@ set -euo pipefail
 
 # Based on https://github.com/jam82/ansible-role-skeleton#usage
 
+SUBMODULE=0
+
+if [[ "$(basename "$0")" = "new-role-submodule.sh" ]]; then
+    SUBMODULE=1
+fi
+
 if [ -z "$1" ]; then
     echo "Please specify a role name that will be appended to ansible-role-<rolename>"
     exit 1
 fi
 
 ansible-galaxy role init --role-skeleton=../ansible-role-skeleton roles/ansible-role-"$1"
-sh roles/ansible-role-"$1"/init.sh
+GIT_INIT="$SUBMODULE" sh roles/ansible-role-"$1"/init.sh
 
 mv roles/ansible-role-"$1" roles/"$1"
+
+if [[ $SUBMODULE -eq 0 ]]; then
+    echo "Not adding submodule"
+    exit 0
+fi
 
 echo "Adding submodule"
 git -C roles/"$1" commit -m "Initial commit"
