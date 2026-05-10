6e866127fe490193346e701a6d30840344bb1b73
Andrei Costescu <andrei@costescu.no>
2026-05-10 08:55:22 +0200

    Borg fixes
    - Indent post_run properly
    - Fix and log more in ZFS hooks

--- files/borg/post_run_zfs.sh
+++ files/borg/post_run_zfs.sh
@@ -6,9 +6,15 @@ while read -r mntpt; do
 
     target_dir="${TMP_ROOT}${mntpt}"
     if mountpoint -q "$target_dir"; then
-        umount "$target_dir"
+        umount -v "$target_dir"
+        tmp_exit=$?
+        global_exit=$(( tmp_exit > global_exit ? tmp_exit : global_exit ))
     fi
 done < <(zfs list -H -o mountpoint -t filesystem -r "$POOL" | tac)
 
 find "$TMP_ROOT" -type d -print -delete
+tmp_exit=$?
+global_exit=$(( tmp_exit > global_exit ? tmp_exit : global_exit ))
 zfs destroy -rv "$POOL@$SNAP_NAME"
+tmp_exit=$?
+global_exit=$(( tmp_exit > global_exit ? tmp_exit : global_exit ))
